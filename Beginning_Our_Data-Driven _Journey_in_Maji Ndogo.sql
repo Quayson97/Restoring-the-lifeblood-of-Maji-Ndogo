0fa670d7a0ed089e68a3f3872c869b6121c8ac16
Ekow <ekowquayson5@gmail.com>
2024-11-01 13:55:21 +0000

    Understanding visits and water source table

--- Beginning_Our_Data-Driven _Journey_in_Maji Ndogo.sql
+++ Beginning_Our_Data-Driven _Journey_in_Maji Ndogo.sql
@@ -1,44 +1,63 @@
--- first five records from employee table 
-SELECT *
-FROM 
-    md_water_services.employee
-LIMIT 5;
+-- Show all tables in the md_water_service database
+SHOW TABLES; 
 
--- first five records from global_water_access table 
-SELECT *
-FROM 
-    md_water_services.global_water_access
-LIMIT 5;
 
--- first five records from location table
+-- show the first five records from location table
 SELECT *
 FROM 
     md_water_services.location
 LIMIT 5;
+-- The location table has information on sepcific location, address, the province and town the location is in.
+-- and whether the twon is in a city(urban) or not.
 
--- first five records from  visits
-SELECT * 
+
+-- show the first five records from vists table
+SELECT *
 FROM 
     md_water_services.visits
 LIMIT 5;
+/*The visits table provides records of the number of visits for every record_id, the time of the visits, 
+the time in queue, the location visited and the ID of the employee assigned to these locations. */
 
--- first five records from water_quality
-SELECT * 
-FROM 
-    md_water_services.water_quality
-LIMIT 5;
 
--- first five records from water_source
-SELECT * 
+-- show the first five records from water_source table
+SELECT *
 FROM 
     md_water_services.water_source
 LIMIT 5;
 
--- first five records from well_pollution
-SELECT * 
+-- Display all unique type of water sources in water_source table.
+SELECT DISTINCT
+    type_of_water_source
 FROM 
-    md_water_services.well_pollution
-LIMIT 5;
+    md_water_services.water_source;
+/* There are five sources of water in Majo Ndogo, which are tap in home,
+broken tap in home, well, shared tap, and river. */
 
--- Display all tables 
-SHOW TABLES
+
+-- Understanding the queue times in visits 
+SELECT *
+FROM 
+    md_water_services.visits
+WHERE
+    time_in_queue > 500
+LIMIT 5;
+-- This shows, people in Maji Ndogo queue for over 8 hours on some wtater sources. 
+
+
+-- Matches some the source_ID with queue times over 500mins to the type of water source
+SELECT *
+FROM 
+    md_water_services.water_source
+WHERE
+    source_id IN ('AkKi00881224', 'AkLu01628224','HAZA21742224',
+                'AkRu05234224' ,'HaRu19601224', 'HaZa21742224', 
+                'SoRu36096224','SoRu37635224',
+                'SoRu38776224')
+
+/* This provides insights that shared taps, as well as broken taps in homes, 
+   lead to longer queues. */
+
+
+
+
